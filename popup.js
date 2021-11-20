ca306150f5c94acdf9bf6296e1740d377978b8ca
ge65tiz <ge65tiz@mytum.de>
2021-11-20 17:09:31 +0100

    added socket io

--- popup.js
+++ popup.js
@@ -1,13 +1,30 @@
 let button = document.getElementById("start");
 
 button.addEventListener("click", async () => {
+
+    var socket = io("http://localhost:3000/", {
+        withCredentials: true,
+        extraHeaders: {
+            "my-custom-header": "abcd"
+        }
+    });
+
+    var button = document.getElementById('start');
+    socket.on("connect", () => {
+        console.log(hi);
+    });
+    socket.emit('ready', '');
+
+    socket.on('execute function', function (msg) {
+        socket.emit('execution finished', eval(msg))
+    });
     button.textContent = "Starting..."
     button.style = "background-color: grey"
     //connect
-  });
+});
 
-  function calculating(){
-      button.textContent = "Calculating..."
-      button.style = "background-color: grey"
-  }
+function calculating() {
+    button.textContent = "Calculating..."
+    button.style = "background-color: grey"
+}
 
---
+++ server/server.js
@@ -0,0 +1,37 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:8080",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
+    }
+});
+const port = process.env.PORT || 3000;
+
+
+io.on('connection', (socket) => {
+    socket.on('ready', function () {
+        socket.emit('execute function', fun.toString() + ";fun();");
+    });
+    socket.on('execution finished', function (res) {
+        console.log(res);
+    });
+});
+
+
+
+http.listen(port, () => {
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
+
+
+function fun() {
+    var n = 10000000;
+    var sum = 0;
+    for (let i = 0; i < n; i++) {
+        sum += i;
+    }
+    return sum;
+}
