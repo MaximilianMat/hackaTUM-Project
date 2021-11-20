36b32f0ccaa2c4f33cb658bb291f7248dd2ce5a1
Maximilian Matscher <maximilian.matscher@tum.de>
2021-11-20 14:39:21 +0100

    Added UI

---
+++ popup.js
@@ -0,0 +1,13 @@
+let button = document.getElementById("start");
+
+button.addEventListener("click", async () => {
+    button.textContent = "Starting..."
+    button.style = "background-color: grey"
+    //connect
+  });
+
+  function calculating(){
+      button.textContent = "Calculating..."
+      button.style = "background-color: grey"
+  }
+
