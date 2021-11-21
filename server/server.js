837f50c6139621f2ce2923cead97955411d3820b
Maximilian Matscher <maximilian.matscher@tum.de>
2021-11-21 10:42:29 +0100

    Comment

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
     cors: {
-        origin: "chrome-extension://kgfleoebffpollncoegofagcmmopeemg",
+        origin: "chrome-extension://knonhcnhggahhcflglbbljpjfflmpcoa",      //change Chrome ID :)
         methods: ["GET", "POST"],
         transports: ['websocket', 'polling'],
         credentials: true
